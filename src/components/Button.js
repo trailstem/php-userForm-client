a513fce44f7a6f8fc425147bbe96dffde47cae77
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-05-11 00:37:44 +0900

    Buttonコンポーネント作成

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+export const Button = (props) => {
+  return (
+    <button
+      className={props.className}
+      type={props.type}
+      onClick={props.onClick}
+    >
+      {props.name}
+    </button>
+  );
+};
+
+export default Button;
